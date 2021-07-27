022f43bbb76cf8825f915331c39ca902c9f900f4
GGunta <msdoors@gmail.com>
2021-07-27 14:50:14 +0300

    without code

--- DayFinder/Controller/ViewController.swift
+++ DayFinder/Controller/ViewController.swift
@@ -8,10 +8,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+  
+    @IBOutlet weak var dayTextField: UITextField!
+    @IBOutlet weak var monthTextField: UITextField!
+    @IBOutlet weak var yearTextField: UITextField!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
 
