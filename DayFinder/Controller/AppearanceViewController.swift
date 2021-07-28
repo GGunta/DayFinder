8a45aea234f7e6b7807c211f161685e0fd6ece42
GGunta <msdoors@gmail.com>
2021-07-28 19:38:18 +0300

    have to finish from lesson xcode5 and fix constraints

---
+++ DayFinder/Controller/AppearanceViewController.swift
@@ -0,0 +1,26 @@
+//
+//  AppearanceViewController.swift
+//  DayFinder
+//
+//  Created by gunta.golde on 28/07/2021.
+//
+
+import UIKit
+
+class AppearanceViewController: UIViewController {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+        
+        @IBAction func closeButtonTapped(_ sender: Any) {
+            self.dismiss(animated: true, completion: nil)
+        }
+        
+    }
+    
+
+  
+
+
